a514df864c2c70adca151d84666601a8a144f507
InGwonKang <tnqkr3494@naver.com>
2025-03-20 21:26:51 +0900

    [no-issue] feat: eslint, prettier, husky 세팅

--- src/services/QueryErrorBoundary.tsx
+++ src/services/QueryErrorBoundary.tsx
@@ -1,13 +1,9 @@
-import { useQueryErrorResetBoundary } from "@tanstack/react-query";
-import { ReactNode } from "react";
-import { ErrorBoundary } from "react-error-boundary";
-import ErrorIcon from "@/assets/Error.svg?react";
+import { useQueryErrorResetBoundary } from '@tanstack/react-query';
+import { ReactNode } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
+import ErrorIcon from '@/assets/Error.svg?react';
 
-export default function QueryErrorBoundary({
-  children,
-}: {
-  children: ReactNode;
-}) {
+export default function QueryErrorBoundary({ children }: { children: ReactNode }) {
   const { reset } = useQueryErrorResetBoundary();
 
   return (
@@ -16,24 +12,22 @@ export default function QueryErrorBoundary({
       fallbackRender={({ resetErrorBoundary }) => (
         <div
           style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-            height: "100%",
-            backgroundColor: "#f8f8f8",
-            textAlign: "center",
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: '100%',
+            backgroundColor: '#f8f8f8',
+            textAlign: 'center',
           }}
         >
-          <ErrorIcon
-            style={{ width: "70px", height: "auto", marginBottom: "20px" }}
-          />
+          <ErrorIcon style={{ width: '70px', height: 'auto', marginBottom: '20px' }} />
 
           <h2
             style={{
-              fontSize: "1.2rem",
-              fontWeight: "bold",
-              marginBottom: "15px",
+              fontSize: '1.2rem',
+              fontWeight: 'bold',
+              marginBottom: '15px',
             }}
           >
             오류 발생!
@@ -41,13 +35,13 @@ export default function QueryErrorBoundary({
           <button
             onClick={resetErrorBoundary}
             style={{
-              backgroundColor: "black",
-              color: "white",
-              padding: "10px 20px",
-              border: "none",
-              borderRadius: "5px",
-              fontSize: "1rem",
-              cursor: "pointer",
+              backgroundColor: 'black',
+              color: 'white',
+              padding: '10px 20px',
+              border: 'none',
+              borderRadius: '5px',
+              fontSize: '1rem',
+              cursor: 'pointer',
             }}
           >
             ↻ 다시 시도
